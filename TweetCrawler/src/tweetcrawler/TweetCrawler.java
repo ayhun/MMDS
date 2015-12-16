e1306f95f01f744c0cd26583e2163b3614ee47ca
ayhun <ayhuntekat@gmail.com>
2015-12-16 14:43:54 +0100

    some more progress

--- TweetCrawler/src/tweetcrawler/TweetCrawler.java
+++ TweetCrawler/src/tweetcrawler/TweetCrawler.java
@@ -5,6 +5,8 @@
  */
 package tweetcrawler;
 
+import java.io.File;
+
 /**
  *
  * @author ayhun
@@ -15,7 +17,26 @@ public class TweetCrawler {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        final String dataFolderName = "tweets";
+        final String productName = "iphone";
+        final String[] keywords = new String[]{"battery", "screen", "camera"};
+        
+        System.out.println("Number of files in folder:" + getFilesCount(new File(dataFolderName)));
+        System.out.println(new File(dataFolderName).listFiles()[1].getAbsolutePath());
     }
-    
+
+    public static int getFilesCount(File file) {
+        File[] files = file.listFiles();
+        int count = 0;
+        for (File f : files) {
+            if (f.isDirectory()) {
+                count += getFilesCount(f);
+            } else if(f.getName().endsWith("bz2")) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
 }
---
+++ TweetCrawler/src/tweetcrawler/Worker.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tweetcrawler;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author ayhun
+ */
+public class Worker implements Runnable {
+    // remember which files already processed
+    private static ConcurrentHashMap<String, String> processedFiles = new ConcurrentHashMap<>();
+    // other vars
+    private final String[] keywords;
+    private final int id;
+    
+    public Worker(String[] keywords, int id){
+        this.keywords = keywords;
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public static synchronized void appendContents(String sFileName, String sContent) {
+        try {
+            File oFile = new File(sFileName+".json");
+            if (!oFile.exists()) {
+                oFile.createNewFile();
+            }
+            if (oFile.canWrite()) {
+                BufferedWriter oWriter = new BufferedWriter(new FileWriter(sFileName, true));
+                oWriter.write(sContent);
+                oWriter.close();
+            }
+        } catch (IOException oException) {
+            throw new IllegalArgumentException("Error appending/File cannot be written: \n" + sFileName);
+        }
+    }
+}
